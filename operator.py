f1f2fc3b8073d76f26be90f4f8e4fbfdce6c6633
Alithebest-coder <aurumprint@proton.me>
2024-11-01 19:09:17 +1000

    Add files via upload

---
+++ operator.py
@@ -0,0 +1,17 @@
+a=10000
+b=1203
+  
+print("sum",a+b)
+print()
+print("difference",a-b)
+print()
+print("product",a*b)
+print()
+print("quotient",a/b)
+print()
+print("floor division",3//2)
+print()
+print("modulo",9%2)
+print() 
+print("exponential power",3**2)
+print()
